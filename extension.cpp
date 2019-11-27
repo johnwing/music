4ad425f91153295cacd54aab8edd34e619a186b0
johnwing <47427803+johnwing@users.noreply.github.com>
2019-11-27 21:05:32 +0800

    Update extension.cpp

--- extension.cpp
+++ extension.cpp
@@ -1,13 +1,10 @@
 #include "pxt.h"
-#include "MicroBit.h"
-MicroBit uBit;
 using namespace pxt;
 namespace midi {
     //%
     int frequencyToKey(int frequency) {
         return max(0, min(127, round(log(frequency/440.0)/log(2) * 12 + 69)));
     }
-    
     //%
     int police()
     {
